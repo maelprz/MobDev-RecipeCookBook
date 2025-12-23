010710cfeabf084108d988975dc6b4dc66dc341b
Mel Stephen Perez <mel.perez.5203@gmail.com>
2025-12-23 09:41:54 +0800

    - removed unused rate and notes model

--- flutter_application_recipebook/lib/Models/notes.dart
+++
@@ -1,6 +0,0 @@
-class UserNotes {
-  final String recipeId;
-  final String note;
-
-  UserNotes({required this.recipeId, required this.note});
-}
--- flutter_application_recipebook/lib/Models/rate.dart
+++
@@ -1,6 +0,0 @@
-class Rating {
-  final String recipeId;
-  final int stars; // 1–5
-
-  Rating({required this.recipeId, required this.stars});
-}
